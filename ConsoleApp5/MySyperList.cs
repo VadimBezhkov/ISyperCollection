18745c440858656f18ef688396879a937d1c954d
Вадим Бежков <vadimbezhkov3112@gmail.com>
2021-02-18 15:27:13 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/MySyperList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class MySyperList : IEnumerable
+    {
+        private string[] _data;
+        public MySyperList()
+        {
+            _data = new string[5];
+        }
+        public void Add(string item)
+        {
+           var corentIndex =_data[_data.Count(i=>i!=null)] = item;
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return MySuperEnumerator();
+        }
+    }
+}
