a90c6630d5aa87eae7c67756980aa93acf2b59da
Вадим Бежков <vadimbezhkov3112@gmail.com>
2021-02-20 13:00:38 +0300

    Fix nullreference exeption!!!

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -16,12 +16,13 @@ namespace ConsoleApp5
             MyCollection.Add(new MySuperCollection("Vadim",5,4));
             MyCollection.Add(new MySuperCollection("Pavel",1,2));
             MyCollection.Add(new MySuperCollection("Timofey",9,8));
-            MyCollection.Add(new MySuperCollection("Ivan", 4, 8));
+            //MyCollection.Add(new MySuperCollection("Ivan", 4, 8));
             IEnumerable enumerable = MyCollection as IEnumerable;
             IEnumerator enumerator = enumerable.GetEnumerator();
             while (enumerator.MoveNext())
             {
                 MySuperCollection myIndex = enumerator.Current as MySuperCollection;
+                if(myIndex!=null)
                 Console.WriteLine("Name {0}, Fild1 {1}, Fild2 {2}", myIndex.Name, myIndex.Fild1, myIndex.Fild2);
             }
             enumerator.Reset();
