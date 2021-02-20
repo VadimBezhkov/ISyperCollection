7896ce43a55ae50417b3f5e1748b44e27dcbc662
Вадим Бежков <vadimbezhkov3112@gmail.com>
2021-02-20 11:56:00 +0300

    fdfd

--- ConsoleApp5/MySyperList.cs
+++ ConsoleApp5/MySyperList.cs
@@ -7,20 +7,33 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp5
 {
-    public class MySyperList : IEnumerable
+    public class MySyperList : IEnumerable, IEnumerator
     {
         private string[] _data;
         public MySyperList()
         {
             _data = new string[5];
         }
+
+        public object Current => throw new NotImplementedException();
+
         public void Add(string item)
         {
            var corentIndex =_data[_data.Count(i=>i!=null)] = item;
         }
         public IEnumerator GetEnumerator()
         {
-            return MySuperEnumerator();
+            return this as IEnumerator;
+        }
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
         }
     }
 }
