d29e173ed4a006d64f73bde1efe59279ec53065f
Вадим Бежков <vadimbezhkov3112@gmail.com>
2021-02-23 11:44:56 +0300

    Update

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -13,6 +13,7 @@ namespace ConsoleApp5
         {
             MySyperList MyCollection = new MySyperList();
             MyCollection.RegisterDelegate(new MySyperList.MyDelegate(Show_Message));
+
             MyCollection.Add(new MySuperCollection("Alexsey",3,5));
             MyCollection.Add(new MySuperCollection("Vadim",5,4));
             MyCollection.Add(new MySuperCollection("Pavel",1,2));
@@ -21,21 +22,23 @@ namespace ConsoleApp5
             MyCollection.Add(new MySuperCollection("Pavel121221", 1, 2));
             MyCollection.Add(new MySuperCollection("Timofey122121", 9, 8));
             MyCollection.Add(new MySuperCollection("Ivan121212", 4, 8));
+
             MyCollection.Remove(2);
             MyCollection.Remove("Pavel121221");
             MyCollection.Remove("Timofey122121");
             MyCollection.Remove("Ivan121212");
             //MyCollection.Remove("Ivan1212qweqw12"); // раскоментировать для теста
+            Console.WriteLine();
+
             IEnumerable enumerable = MyCollection as IEnumerable;
             IEnumerator enumerator = enumerable.GetEnumerator();
+
             while (enumerator.MoveNext())
             {
                 MySuperCollection myIndex = enumerator.Current as MySuperCollection;
                 if (myIndex != null)
                     Console.WriteLine("Name {0}, Fild1 {1}, Fild2 {2}", myIndex.Name, myIndex.Fild1, myIndex.Fild2);
-                
             }
-
             Console.ReadKey();
         }
         private static void Show_Message(string message)
